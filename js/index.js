62eaa606290084fd17a7f8a67da1c3f4f540f069
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-16 17:18:42 +0300

    ready

--- js/index.js
+++ js/index.js
@@ -1,25 +1,11 @@
 'use strict';
-import Swiper from './modules/swiper/swiper-bundle.esm.browser.min.js';
+const checkBoxRef = document.querySelector('#burger');
+const burgerRef = document.querySelector('.header__nav-burger-box');
+const bodyRef = document.querySelector('body');
 
-// const swiper = new Swiper(...)
+burgerRef.addEventListener('click', burgerHolder);
 
-var mySwiper = new Swiper('.swiper-container', {
-  // // Optional parameters
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.control__list',
-    bulletClass: 'control__item',
-    bulletActiveClass: 'control__item--active',
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.control__next-img',
-    prevEl: '.control__prev-img',
-  },
-  //   // And if we need scrollbar
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //   },
-});
-console.log(mySwiper);
+function burgerHolder(event) {
+  const overflow = checkBoxRef.checked ? 'visible' : 'hidden';
+  bodyRef.style.overflow = overflow;
+}
