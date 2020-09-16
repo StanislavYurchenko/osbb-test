29e506cbd58c0a66b1ff72a07c18e852f35848ef
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-09-16 11:17:05 +0300

    before swiper

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+import Swiper from './modules/swiper/swiper-bundle.esm.browser.min.js';
+
+// const swiper = new Swiper(...)
+
+var mySwiper = new Swiper('.swiper-container', {
+  // // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+  // // If we need pagination
+  // pagination: {
+  //       el: '.swiper-pagination',
+  // },
+  // // Navigation arrows
+  // navigation: {
+  //       nextEl: '.swiper-button-next',
+  //       prevEl: '.swiper-button-prev',
+  // },
+  // // And if we need scrollbar
+  // scrollbar: {
+  //       el: '.swiper-scrollbar',
+  // },
+});
+console.log(mySwiper);
